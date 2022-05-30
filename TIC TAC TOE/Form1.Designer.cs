0909631dd41a50ef5bd3b9fdfc2a88b4846044cc
Bryce Madelo <brycemadelo07@gmail.com>
2022-05-30 18:03:35 +0800

    Layout

---
+++ TIC TAC TOE/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace TIC_TAC_TOE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.p1 = new System.Windows.Forms.Button();
+            this.p2 = new System.Windows.Forms.Button();
+            this.p3 = new System.Windows.Forms.Button();
+            this.p4 = new System.Windows.Forms.Button();
+            this.p5 = new System.Windows.Forms.Button();
+            this.p6 = new System.Windows.Forms.Button();
+            this.p7 = new System.Windows.Forms.Button();
+            this.p8 = new System.Windows.Forms.Button();
+            this.p9 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mENUToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mechanicsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // p1
+            // 
+            this.p1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.p1.FlatAppearance.BorderSize = 2;
+            this.p1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.p1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1.Location = new System.Drawing.Point(31, 77);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(66, 56);
+            this.p1.TabIndex = 0;
+            this.p1.UseVisualStyleBackColor = true;
+            // 
+            // p2
+            // 
+            this.p2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.p2.FlatAppearance.BorderSize = 2;
+            this.p2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.p2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2.Location = new System.Drawing.Point(103, 77);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(66, 56);
+            this.p2.TabIndex = 1;
+            this.p2.UseVisualStyleBackColor = true;
+            // 
+            // p3
+            // 
+            this.p3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.p3.FlatAppearance.BorderSize = 2;
+            this.p3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.p3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p3.Location = new System.Drawing.Point(175, 77);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(66, 56);
+            this.p3.TabIndex = 2;
+            this.p3.UseVisualStyleBackColor = true;
+            // 
+            // p4
+            // 
+            this.p4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.p4.FlatAppearance.BorderSize = 2;
+            this.p4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.p4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p4.Location = new System.Drawing.Point(31, 139);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(66, 56);
+            this.p4.TabIndex = 3;
+            this.p4.UseVisualStyleBackColor = true;
+            // 
+            // p5
+            // 
+            this.p5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.p5.FlatAppearance.BorderSize = 2;
+            this.p5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.p5.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p5.Location = new System.Drawing.Point(103, 139);
+            this.p5.Name = "p5";
+            this.p5.Size = new System.Drawing.Size(66, 56);
+            this.p5.TabIndex = 4;
+            this.p5.UseVisualStyleBackColor = true;
+            // 
+            // p6
+            // 
+            this.p6.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.p6.FlatAppearance.BorderSize = 2;
+            this.p6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.p6.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p6.Location = new System.Drawing.Point(175, 139);
+            this.p6.Name = "p6";
+            this.p6.Size = new System.Drawing.Size(66, 56);
+            this.p6.TabIndex = 5;
+            this.p6.UseVisualStyleBackColor = true;
+            // 
+            // p7
+            // 
+            this.p7.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.p7.FlatAppearance.BorderSize = 2;
+            this.p7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.p7.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p7.Location = new System.Drawing.Point(31, 200);
+            this.p7.Name = "p7";
+            this.p7.Size = new System.Drawing.Size(66, 56);
+            this.p7.TabIndex = 6;
+            this.p7.UseVisualStyleBackColor = true;
+            // 
+            // p8
+            // 
+            this.p8.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.p8.FlatAppearance.BorderSize = 2;
+            this.p8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.p8.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p8.Location = new System.Drawing.Point(103, 200);
+            this.p8.Name = "p8";
+            this.p8.Size = new System.Drawing.Size(66, 56);
+            this.p8.TabIndex = 7;
+            this.p8.UseVisualStyleBackColor = true;
+            // 
+            // p9
+            // 
+            this.p9.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.p9.FlatAppearance.BorderSize = 2;
+            this.p9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.p9.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p9.Location = new System.Drawing.Point(175, 200);
+            this.p9.Name = "p9";
+            this.p9.Size = new System.Drawing.Size(66, 56);
+            this.p9.TabIndex = 8;
+            this.p9.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackgroundImage = global::TIC_TAC_TOE.Properties.Resources._1920x1080_light_green_solid_color_background;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mENUToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(273, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mENUToolStripMenuItem
+            // 
+            this.mENUToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mechanicsToolStripMenuItem,
+            this.restartToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.mENUToolStripMenuItem.Name = "mENUToolStripMenuItem";
+            this.mENUToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.mENUToolStripMenuItem.Text = "MENU";
+            // 
+            // mechanicsToolStripMenuItem
+            // 
+            this.mechanicsToolStripMenuItem.Name = "mechanicsToolStripMenuItem";
+            this.mechanicsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.mechanicsToolStripMenuItem.Text = "Mechanics";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 33);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "TIC TAC TOE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TIC_TAC_TOE.Properties.Resources._1920x1080_light_green_solid_color_background;
+            this.ClientSize = new System.Drawing.Size(273, 271);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.p9);
+            this.Controls.Add(this.p8);
+            this.Controls.Add(this.p7);
+            this.Controls.Add(this.p6);
+            this.Controls.Add(this.p5);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button p1;
+        private System.Windows.Forms.Button p2;
+        private System.Windows.Forms.Button p3;
+        private System.Windows.Forms.Button p4;
+        private System.Windows.Forms.Button p5;
+        private System.Windows.Forms.Button p6;
+        private System.Windows.Forms.Button p7;
+        private System.Windows.Forms.Button p8;
+        private System.Windows.Forms.Button p9;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mENUToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mechanicsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
